5566f6803364de65e399a01132cb45362e0652f9
pakjea-sure <129921224+pakjea-sure@users.noreply.github.com>
2023-09-12 16:06:23 +0900

    caas key 출력부 제거

--- build_caas.sh
+++ build_caas.sh
@@ -1,5 +1,4 @@
 echo "Coverage Maven Build..."
 echo "mvn com.qualityscroll.caas:jacov-maven-plugin:1.0.0-SNAPSHOT:setup $* -Dmaven.jacov.addDependencyJavaLib=true"
-echo KEY - ${CAAS_API_KEY}
 mvn com.qualityscroll.caas:jacov-maven-plugin:1.0.0-SNAPSHOT:setup $* -Dmaven.jacov.addDependencyJavaLib=true
 echo "Build Finished"
