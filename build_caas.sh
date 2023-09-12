f960e7076e44e1ed93ff3f2693254f6e6f10134c
pakjea-sure <129921224+pakjea-sure@users.noreply.github.com>
2023-09-12 15:48:14 +0900

    key값 출력 추가
    build_caas.sh 수정

--- build_caas.sh
+++ build_caas.sh
@@ -1,5 +1,5 @@
 echo "Coverage Maven Build..."
 echo "mvn com.qualityscroll.caas:jacov-maven-plugin:1.0.0-SNAPSHOT:setup $* -Dmaven.jacov.addDependencyJavaLib=true"
-echo ${CAAS_API_KEY}
+echo KEY - ${CAAS_API_KEY}
 mvn com.qualityscroll.caas:jacov-maven-plugin:1.0.0-SNAPSHOT:setup $* -Dmaven.jacov.addDependencyJavaLib=true
 echo "Build Finished"
